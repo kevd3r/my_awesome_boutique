ca584aae5119b0cba88ee428c7859f7d91a7f461
kevd3r <64579607+kevd3r@users.noreply.github.com>
2021-09-13 11:39:06 +0200

    changes added in index.html

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+let button = document.getElementById('menu-button');
+let nav = document.getElementById('hideAndShow');
+
+button.addEventListener('click', function(e){
+    this.classList.toggle('is-open');
+    nav.classList.toggle('is-open');
+});
