49da4ac0edd13be5880616a8a1f086fc200478d6
kevd3r <64579607+kevd3r@users.noreply.github.com>
2021-09-15 18:03:30 +0200

    added a js function not ended yet

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,39 @@ button.addEventListener('click', function(e){
     nav.classList.toggle('is-open');
 });
 
-let item = document.getElementsByName('menu-item');
+
+let url = window.location.pathname;
+console.log(url);
+let tmp = [];
+let table = document.querySelector('div.shown_menu>div>a');
+let tables = document.querySelectorAll('div.shown_menu>div>a');
+console.log(table);
+console.log(tables);
+tables.forEach(function(el){
+    console.log(el);
+    tmp.push(el.getAttribute('href'));  
+});
+console.log(tmp);
 
 
-console.log(item.innerText);
+
+
+
+Array.from(table);
+console.log(table[1]);
+
+
+
+/*
+function currentPage(){
+for (let value of item){
+    if (value == url){
+        /*parentNode.item[i].style.background-color="pink";
+        console.log(value);
+    }
+}
+}
+jQuery(function($){
+    console.log('jQuery est inclus! hosanna !!')
+    
+});*/
