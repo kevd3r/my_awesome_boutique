641d6a971ff7741827682d21b41d52ef1be2f337
kevd3r <64579607+kevd3r@users.noreply.github.com>
2021-09-22 16:03:31 +0200

    added canvas' configuration with good ids

--- scripts/script.js
+++ scripts/script.js
@@ -9,37 +9,34 @@ button.addEventListener('click', function(e){
 
 /*------------- changes current's page aspect------------- */
 let url = window.location.pathname;
-console.log(url);
 let tmp = [];
 let table = document.querySelector('div.shown_menu>div>a');
 let tables = document.querySelectorAll('div.shown_menu>div>a');
-console.log(table);
-console.log(tables);
 tables.forEach(function(el){
-    console.log(el);
     tmp.push(el.getAttribute('href'));  
 });
 
 let indice = 0;
 tmp.forEach(function(element){
   if (element == url){
-    console.log(element);
     indice = tmp.indexOf(element);
-    console.log(tmp.indexOf(element));
-    console.log(tables[indice]);
     tables[indice].parentNode.style.backgroundColor ='#464646';
   }
 });
 
-/*------------------data's submitting form------------------- */
-var initials;
-function afficher(){
+/*------------------data's submitting form displays initials------------------ */
+let contactBtn = document.getElementById('contact-button');
+
+contactBtn.addEventListener('click', function affiche(){
   let firstname = document.getElementById('firstname').value;
   let lastname = document.getElementById('lastname').value;
-  let tmp2=[firstname, lastname];
-  console.log(tmp2);
   firstname = firstname.charAt(0).toUpperCase();
   lastname = lastname.charAt(0).toUpperCase();
+  let initials= firstname+lastname;
+  console.log(initials);
+  localStorage.setItem('user',JSON.stringify(initials));
+  user = JSON.parse(localStorage.getItem('user'));
+  console.log(user);
   let newspan = document.createElement('span');
   let f = document.querySelector('div.form-container');
     newspan.textContent = 'Bonjour'+' '+firstname+ ' ' +lastname;
@@ -49,24 +46,33 @@ function afficher(){
   f.append(newspan);
   let c = document.querySelector('form.contact');
   c.style.display = 'none';
-  initials = firstname+lastname;
-}
-console.log(initials);
-const canvas = document.getElementById('contact_initials');
-let sizeCanvas = canvas.getAttribute('width')
-let ctx;
-if (canvas.getContext){
-  ctx = canvas.getContext('2d');
-  console.log(sizeCanvas);
-  ctx.beginPath();
-  ctx.arc(600, 500, sizeCanvas/2, 0, Math.PI*2);
-  ctx.stroke();
-  const sizeText = sizeCanvas/3;
-  ctx.fillStyle = 'cadetblue';
-  ctx.textAlign = 'center';
-  ctx.textBaseline = 'middle';
-  ctx.font =`${sizeText}vh Arial`;
-  ctx.fillText('FF', sizeCanvas/2, sizeCanvas/2);
-}else{
+  showInitials();
+  })
+
+function showInitials (){
+  let user =JSON.parse(localStorage.getItem('user'));
+  console.log(user);
+  if (user!=null){
+
+      let canvas = document.getElementById('canvas');
+      let sizeCanvas = canvas.getAttribute('width');
+      let ctx;
+        if (canvas.getContext){
+          ctx = canvas.getContext('2d');
+          ctx.beginPath();
+          ctx.arc(sizeCanvas/2, sizeCanvas/2, sizeCanvas/2, 0, Math.PI*2);
+          ctx.fillStyle = "grey";
+          ctx.fill();
+          const sizeText = sizeCanvas/3;
+          ctx.fillStyle = 'orange';
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'middle';
+          ctx.font =`${sizeText}px Arial`;
+          ctx.fillText(user, sizeCanvas/2, sizeCanvas/2);
+    }else{
   console.log('canevas non supporté');
 }
+}else{
+  canvas.style.display= none;
+}
+};
